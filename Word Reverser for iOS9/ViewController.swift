a4f8b6250c7be3392054d899d5bf8672a0a290e0
Nurulazrad Murad <nurulazrad@gmail.com>
2016-10-11 12:55:33 +0800

    Hooking up the actions and the outlets

--- Word Reverser for iOS9/ViewController.swift
+++ Word Reverser for iOS9/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblReverseWord: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnReverse(sender: UIButton) {
+    }
+    
+    @IBAction func btnClear(sender: UIButton) {
     }
 
 
