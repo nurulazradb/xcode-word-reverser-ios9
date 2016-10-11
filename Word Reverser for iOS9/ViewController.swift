7a41fe2be01557d71f0f7305e014d864582dda25
Nurulazrad Murad <nurulazrad@gmail.com>
2016-10-11 12:39:55 +0800

    Initial Commit

---
+++ Word Reverser for iOS9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Reverser for iOS9
+//
+//  Created by Nurulazrad Bin Murad on 10/11/16.
+//  Copyright © 2016 Herbalife Product. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
